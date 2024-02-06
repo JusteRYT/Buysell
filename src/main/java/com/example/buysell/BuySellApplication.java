15cc0b652c604d0aaceea980f2134d3ffc910a09
Ivan Ivanov <93572822+JusteRYT@users.noreply.github.com>
2024-02-06 18:33:03 +0300

    Initial commit

---
+++ src/main/java/com/example/buysell/BuySellApplication.java
@@ -0,0 +1,12 @@
+package com.example.buysell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuySellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuySellApplication.class, args);
+	}
+}
